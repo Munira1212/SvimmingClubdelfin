8d1b407c208fc228bcc3256d63951ea5135e9a20
Munira1212 <muni0301@stud.kea.dk>
2022-11-25 09:46:13 +0100

    Initial commit

---
+++ src/Junior.java
@@ -0,0 +1,7 @@
+public class Junior extends Svimmingdiciplin {
+
+
+    public Junior(String name, String placering, int time, String diciplin) {
+        super(name, placering, time, diciplin);
+    }
+}
---
+++ src/Senior.java
@@ -0,0 +1,7 @@
+public class Senior extends Svimmingdiciplin {
+
+
+    public Senior(String name, String placering, int time, String diciplin) {
+        super(name, placering, time, diciplin);
+    }
+}
